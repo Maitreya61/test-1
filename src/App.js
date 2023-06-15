b99b901eb0be70889fa0f2ee16e01cfdb55cdb7a
Maitreya61 <maitreyaoffl@gmail.com>
2023-06-15 11:51:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
+
+  const [workouts,setWorkouts] = useState([])
+
+  useEffect(()=>{
+    axios.get("/workouts")
+     .then((res) => setWorkouts(res.data))
+ },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="workouts">
+        {workouts && workouts.map((workout) => {
+          return (
+            <div className="workout-details" key={workout._id}>
+              <h4>{workout.title}</h4>
+              <p><strong>Load (kg): </strong>{workout.load}</p>
+              <p><strong>Number of reps: </strong>{workout.reps}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
